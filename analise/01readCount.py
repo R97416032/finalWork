3dbf85e7408367925a0f5606094978933f7940b7
R <97416032@qq.com>
2023-11-28 14:20:16 +0800

    获取miRNA,coding,lncRNA

--- analise/01readCount.py
+++ analise/01readCount.py
@@ -8,12 +8,11 @@ def read(cellinfo_path,geneinfo_path,counts_path):
     adata.obs_names.name = 'cell'
     adata.var.index = adata.var['gene']
     adata.var_names.name = 'gene'
+    adata.write(cellinfo_path.split("ungz/")[0]+"h5ad/all.h5ad")
     cd56hcd16l=adata[adata.obs["Majortype"]=="CD56highCD16low"]
     cd56lcd16h=adata[adata.obs["Majortype"]=="CD56lowCD16high"]
     cd56lcd16h.write(cellinfo_path.split("ungz/")[0]+"h5ad/cd56lcd16h.h5ad")
     cd56hcd16l.write(cellinfo_path.split("ungz/")[0]+"h5ad/cd56hcd16l.h5ad")
-
-
 cellinfo_path="../data/raw/gse212890/ungz/GSE212890_NK_metadata.csv"
 geneinfo_path="../data/raw/gse212890/ungz/GSE212890_NK.genes.csv"
 counts_path="../data/raw/gse212890/ungz/GSE212890_NK_counts.mtx"
