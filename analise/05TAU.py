9272de4a6341bb1a80ed08a83f207b3b6d7b759c
R <97416032@qq.com>
2023-12-19 09:10:59 +0800

    计算tau

--- analise/05TAU.py
+++ analise/05TAU.py
@@ -3,26 +3,47 @@ import scanpy as sc
 import numpy as np
 from matplotlib import pyplot as plt
 
-cd56lcd16h_path="../data/raw/gse212890/h5ad/cd56lcd16h_gauss_knn_neigh100_pc10.h5ad"
-cd56hcd16l_path="../data/raw/gse212890/h5ad/cd56hcd16l_gauss_noknn_neigh100_pc10.h5ad"
+cd56lcd16h_path="../data/raw/gse212890/h5ad/cd56lcd16h.h5ad"
+cd56hcd16l_path="../data/raw/gse212890/h5ad/cd56hcd16l.h5ad"
 def tau(path):
     adata=sc.read_h5ad(path)
-    tau=[]
+    tau_mean=[]
+    tau_median=[]
     genes=adata.var["gene"]
-    clusters=list(set(adata.obs["leiden"]))
+    clusters=list(set(adata.obs["celltype"]))
     clusters.sort()
-    print()
+    print(clusters)
+    vars = []
     for g in genes:
         mean=[]
+        median=[]
+        vars.append(np.var(adata[:,adata.var["gene"]==g].X.toarray()))
         for c in clusters:
-            mean.append(np.mean(adata[adata.obs["leiden"]==c,adata.var["gene"]==g].X))
+            mean.append(np.mean(adata[adata.obs["celltype"]==c,adata.var["gene"]==g].X.toarray()))
+            median.append(np.median(adata[adata.obs["celltype"] == c, adata.var["gene"] == g].X.toarray()))
         a=1/(len(clusters)-1)
-        maxi=np.max(mean)
-        tau.append(a*(len(clusters)-np.sum(mean)/maxi))
+        maxmean=np.max(mean)
+        maxmedian=np.max(mean)
+        if maxmean!=0:
+            tau_mean.append(a*(len(clusters)-np.sum(mean)/maxmean))
+        else:
+            tau_mean.append(0)
+        if maxmedian!=0:
+            tau_median.append(a*(len(clusters)-np.sum(median)/maxmedian))
 
-    adata.var["tau"]=tau
-    adata.var["tau"].replace(-np.inf,0,inplace=True)
+        else:
+            tau_median.append(0)
+    adata.var["tau_usemean"]=tau_mean
+    adata.var["tau_usemedian"]=tau_median
+    adata.var["tau_usemean"].replace(-np.inf,0,inplace=True)
+    adata.var["tau_usemedian"].replace(-np.inf,0,inplace=True)
+    print(vars)
+    print(adata)
+    print(len(vars))
+    adata.var["vars"]=vars
 
-    adata.var.to_csv(path.replace('.h5ad','_var.csv'))
-    adata.write(path.replace(".h5ad","_tau.h5ad"))
+
+    adata.var.to_csv(path.replace('.h5ad', '_tau_var.csv').replace("/h5ad/","/h5ad/tau/"))
+    adata.write(path.replace(".h5ad", "_tau.h5ad").replace("/h5ad/","/h5ad/tau/"))
 tau(cd56lcd16h_path)
+tau(cd56hcd16l_path)
