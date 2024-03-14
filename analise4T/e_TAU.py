f1547528ec2f14202683f7e64a3d5cc6bb0d760e
R <97416032@qq.com>
2024-03-14 11:07:51 +0800

    增加了方差限制

--- analise4T/e_TAU.py
+++ analise4T/e_TAU.py
@@ -9,7 +9,6 @@ from matplotlib import pyplot as plt
 
 def tau(path):
     adata=sc.read_h5ad(path)
-    print(adata)
     # sc.pl.highest_expr_genes(adata, n_top=20, )
     sc.pp.filter_cells(adata, min_genes=200)
     sc.pp.filter_genes(adata, min_cells=3)
@@ -17,23 +16,21 @@ def tau(path):
     sc.pp.log1p(adata)
     sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
     adata.raw = adata
-    print(adata)
     tau_mean=[]
-    tau_median=[]
+    # tau_median=[]
     tau_cluster=[]
     genes=adata.var["gene"]
     clusters=list(set(adata.obs["meta.cluster"]))
     clusters.sort()
-    print(clusters)
     vars = []
     for g in genes:
         mean=[]
-        median=[]
+        # median=[]
 
         vars.append(np.var(adata[:,adata.var["gene"]==g].X.toarray()))
         for c in clusters:
             mean.append(np.mean(adata[adata.obs["meta.cluster"]==c,adata.var["gene"]==g].X.toarray()))
-            median.append(np.median(adata[adata.obs["meta.cluster"] == c, adata.var["gene"] == g].X.toarray()))
+            # median.append(np.median(adata[adata.obs["meta.cluster"] == c, adata.var["gene"] == g].X.toarray()))
         a=1/(len(clusters)-1)
         maxmean=np.max(mean)
         # maxmedian=np.max(mean)
@@ -48,17 +45,14 @@ def tau(path):
         # else:
         #     tau_median.append(0)
     adata.var["tau_usemean"]=tau_mean
-    adata.var["tau_usemedian"]=tau_median
+    # adata.var["tau_usemedian"]=tau_median
     adata.var["tau_cluster"] = tau_cluster
     adata.var["tau_usemean"].replace(-np.inf,0,inplace=True)
-    adata.var["tau_usemedian"].replace(-np.inf,0,inplace=True)
+    # adata.var["tau_usemedian"].replace(-np.inf,0,inplace=True)
     # print(vars)
-    print(adata)
-    print(len(vars))
     adata.var["vars"]=vars
-    print(adata)
-    adata.var.to_csv(path.replace('.h5ad', '_tau_var.csv').replace("/qch5ad/","/qch5ad_tau/"),index=None)
-    adata.write(path.replace(".h5ad", "_tau.h5ad").replace("/qch5ad/","/qch5ad_tau/"))
+    adata.var.to_csv(path.replace('.h5ad', '_tau_var.csv').replace("/qch5ad/","/qch5ad_tau/csv/"),index=None)
+    adata.write(path.replace(".h5ad", "_tau.h5ad").replace("/qch5ad/","/qch5ad_tau/h5ad/"))
 
 
 def see_distribution(path,mean):
